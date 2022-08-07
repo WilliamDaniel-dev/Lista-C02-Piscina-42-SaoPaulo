3e2fd8df05031174fb37fe0ff2ce642246133f3b
William Daniel <williandan07@gmail.com>
2022-08-07 17:12:17 -0300

    Terceira lista de C.

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wdaniel- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 13:07:35 by wdaniel-          #+#    #+#             */
+/*   Updated: 2022/07/28 02:45:46 by wdaniel-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while ((src[i] != '\0') && (i < n))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+int main()
+{
+	char *src = "Eu sou mesmo";
+	char dest[20];
+	size_t n = 8;
+
+	printf("%s\n", ft_strncpy(dest, src, n));
+			return (0);
+}
